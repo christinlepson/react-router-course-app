00f07d531fbf4cb3a46144c72408038172efac1e
Christin Nadya Lepson <christin@protonmail.com>
2018-10-31 14:45:24 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Home from './Home';
+
+const App = () => (
+    <BrowserRouter>
+        <div className="container">
+            <Route path='/' component={Home}/>
+        </div>
+    </BrowserRouter>
+);
+
+export default App;
